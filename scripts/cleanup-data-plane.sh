f0c78041d9faa2d6ebadebe51252cb054b30e7a9
Adi Suresh <adsuresh@amazon.com>
2023-04-04 15:37:55 -0500

    Update cleanup-data-plane to also clean up vpce.
    The VPC endpoints must be deleted through Opensearch console first. Once
    deleted, if there are any straggling ENIs, then those must be deleted as
    well.
    
    cr: https://code.amazon.com/reviews/CR-88759582

--- scripts/cleanup-data-plane.sh
+++ scripts/cleanup-data-plane.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
+trap "echo; exit" INT
+
 # beta pdx
-AWS_REGION="us-west-2"
-AWS_ACCOUNT="568243693831"
+AWS_REGION="us-east-1"
+AWS_ACCOUNT="701817888140"
 AWS_ROLE="Admin"
-CLUSTER_ARN="FizzyDrPepper-EcsCluster-beta-us-west-2-cell1-ClusterEB0386A7-ONeX6WBBCaOI"
-CODE_DEPLOY_APPLICATION="FizzyDrPepper-CodeDeploy-beta-us-west-2-cell1-CodeDeployApplicationE587C27C-1N81S1EQ1THQJ"
-DEFAULT_VPC="vpc-0101c717279e5c7d3"
-BASE_VPC="vpc-0a1a6280e2983da95"
+CLUSTER_ARN="FizzyDrPepper-EcsCluster-beta-us-east-1-cell1-ClusterEB0386A7-hrV96abetIpU"
+CODE_DEPLOY_APPLICATION="FizzyDrPepper-CodeDeploy-beta-us-east-1-cell1-CodeDeployApplicationE587C27C-1IHEUWX7XA7YG"
+DEFAULT_VPC="vpc-4b0abe36"
+BASE_VPC="vpc-0c62aec1df1c7368b"
 
 ada credentials update --account=$AWS_ACCOUNT --provider=isengard --role=$AWS_ROLE --once
 
-AWS="aws --region $AWS_REGION "
+AWS="aws --no-paginate --no-cli-pager --no-cli-auto-prompt --region $AWS_REGION "
 
 DESIRED_COUNT="0"
 SERVICE_QUERY='.service | .serviceName + " " + .status'
 
-EXAMPLE_LIST=(
-    "item a" \
-    "item b"
-)
-
 DEPLOYMENT_GROUP_LIST=($($AWS deploy list-deployment-groups --application-name "$CODE_DEPLOY_APPLICATION" | jq '.deploymentGroups[]' | tr -d '"'))
-for DEPLOYMENT_GROUP in "${DEPLOYMENT_GROUP_LIST[@]}"
-do
+for DEPLOYMENT_GROUP in "${DEPLOYMENT_GROUP_LIST[@]}"; do
     echo "Deleting deployment group ${DEPLOYMENT_GROUP}"
     $AWS deploy delete-deployment-group --application-name "$CODE_DEPLOY_APPLICATION" --deployment-group-name "${DEPLOYMENT_GROUP}"
 done
 
+OPENSEARCH_VPCE_LIST=($($AWS opensearch list-vpc-endpoints | jq '.VpcEndpointSummaryList | .[] | .VpcEndpointId' | tr -d '"'))
+for OPENSEARCH_VPCE in "${OPENSEARCH_VPCE_LIST[@]}"; do
+    $AWS opensearch delete-vpc-endpoint --vpc-endpoint-id $OPENSEARCH_VPCE
+done
 
 VPCE_CONNECTION_MAP="$($AWS ec2 describe-vpc-endpoint-connections | jq '.VpcEndpointConnections | map({id: .VpcEndpointId, service: .ServiceId})')"
 VPCE_SERVICE_LIST=($(echo "${VPCE_CONNECTION_MAP}" | jq '.[] | .service' | tr -d '"'))
-for VPCE_SERVICE in "${VPCE_SERVICE_LIST[@]}"
-do
+for VPCE_SERVICE in "${VPCE_SERVICE_LIST[@]}"; do
     echo "Rejecting VPCE connection for service ${VPCE_SERVICE}"
     VPCE_ENDPOINT_LIST=($(echo "${VPCE_CONNECTION_MAP}" | jq ".[] | select(.service == \"${VPCE_SERVICE}\") | .id" |  tr -d '"'))
-    for VPCE_ENDPOINT in "${VPCE_ENDPOINT_LIST[@]}"
-    do
+    for VPCE_ENDPOINT in "${VPCE_ENDPOINT_LIST[@]}"; do
         echo "Connections to reject: ${VPCE_ENDPOINT}"
         $AWS ec2 reject-vpc-endpoint-connections --service-id "${VPCE_SERVICE}" --vpc-endpoint-ids "${VPCE_ENDPOINT}" || true
     done
@@ -45,24 +43,23 @@ do
     $AWS ec2 delete-vpc-endpoint-service-configurations --service-ids "${VPCE_SERVICE}"
 done
 
+VPCE_LIST=($(aws ec2 describe-vpc-endpoints | jq '.VpcEndpoints[].VpcEndpointId' | tr -d '"'))
+for VPCE in "${VPCE_LIST[@]}"; do
+    $AWS ec2 delete-vpc-endpoints --vpc-endpoint-ids $VPCE
+done
 
-#NLB_LIST_FILE=$(mktemp)
-
-# Step 1
 NLB_LIST=($($AWS elbv2 describe-load-balancers \
     | jq '.LoadBalancers[] | .LoadBalancerArn' \
     | tr -d '"' | tr "\n" ' '))
 
-for NLB in "${NLB_LIST[@]}"
-do
+for NLB in "${NLB_LIST[@]}"; do
     echo "Processing NLB ${NLB}"
 
     LISTENER_LIST=($($AWS elbv2 describe-listeners --load-balancer-arn "${NLB}" \
         | jq '.Listeners[] | .ListenerArn' \
         | tr -d '"'))
 
-    for LISTENER in "${LISTENER_LIST[@]}"
-    do
+    for LISTENER in "${LISTENER_LIST[@]}"; do
         echo "Deleting listener ${LISTENER}"
         $AWS elbv2 delete-listener --listener-arn "${LISTENER}"
     done
@@ -76,16 +73,14 @@ CLUSTER_LIST=($($AWS ecs list-clusters \
     | jq '.clusterArns | .[]' \
     | tr -d '"' | tr "\n" ' '))
 
-for CLUSTER in "${CLUSTER_LIST[@]}"
-do
+for CLUSTER in "${CLUSTER_LIST[@]}"; do
     echo "Processing cluster ${CLUSTER}"
 
     SERVICE_LIST=($($AWS ecs list-services --cluster "${CLUSTER_ARN}" \
         | jq '.serviceArns | .[]' \
         | tr -d '"'))
-    
-    for SERVICE in "${SERVICE_LIST[@]}"
-    do
+
+    for SERVICE in "${SERVICE_LIST[@]}"; do
         echo "Updating service ${SERVICE} to desired count ${DESIRED_COUNT}"
         $AWS ecs update-service --desired-count "${DESIRED_COUNT}" --cluster "${CLUSTER}" --service "${SERVICE}" \
             | jq "${SERVICE_QUERY}"
@@ -98,10 +93,33 @@ done
 TARGET_GROUP_LIST=($($AWS elbv2 describe-target-groups \
     | jq '.TargetGroups | .[] | .TargetGroupArn' \
     | tr -d '"'))
-for TARGET_GROUP in "${TARGET_GROUP_LIST[@]}"
-do
+for TARGET_GROUP in "${TARGET_GROUP_LIST[@]}"; do
     echo "Deleting target group ${TARGET_GROUP}"
     $AWS elbv2 delete-target-group --target-group-arn "${TARGET_GROUP}"
+done
+
+TASK_DEFINITION_LIST=($($AWS ecs list-task-definitions | jq '.taskDefinitionArns[] ' | tr -d '"'))
+for TASK_DEFINITION in "${TASK_DEFINITION_LIST[@]}"; do
+    echo "deregistering $TASK_DEFINITION"
+    $AWS ecs deregister-task-definition --task-definition "$TASK_DEFINITION"
+    echo "deleting $TASK_DEFINITION"
+    $AWS ecs delete-task-definitions --task-definitions "$TASK_DEFINITION"
+done
+
+# ENIs
+ENI_ATTACHMENT_LIST=(
+    $($AWS ec2 describe-network-interfaces \
+        --filters "Name=attachment.status,Values=attached" \
+        --query 'NetworkInterfaces[*].[Attachment.AttachmentId]' --output text))
+for ENI_ATTACHMENT in "${ENI_ATTACHMENT_LIST[@]}"; do
+    $AWS ec2 detach-network-interface --attachment-id $ENI_ATTACHMENT --force
+done
+
+ENI_LIST=(
+    $($AWS ec2 describe-network-interfaces \
+        --query 'NetworkInterfaces[*].[NetworkInterfaceId]' --output text))
+for ENI in "${ENI_LIST[@]}"; do
+    $AWS ec2 delete-network-interface --network-interface-id $ENI
 done
 
 SECURITY_GROUP_LIST=($(\
@@ -109,8 +127,7 @@ SECURITY_GROUP_LIST=($(\
         | jq '.SecurityGroups | .[] | select(.IpPermissions[0].FromPort = 21891) | .GroupId' \
         | tr -d '"' \
 ))
-for SECURITY_GROUP in "${SECURITY_GROUP_LIST[@]}"
-do
+for SECURITY_GROUP in "${SECURITY_GROUP_LIST[@]}"; do
     echo "Deleting security group ${SECURITY_GROUP}"
     $AWS ec2 delete-security-group --group-id "${SECURITY_GROUP}"
 done
@@ -120,11 +137,9 @@ IAM_ROLE_LIST=($(\
         | jq '.Roles | map(select(.RoleName | endswith("-cp-role")) | .RoleName) | .[]' \
         | tr -d '"' \
 ))
-for IAM_ROLE in "${IAM_ROLE_LIST[@]}"
-do
+for IAM_ROLE in "${IAM_ROLE_LIST[@]}"; do
     POLICY_NAME_LIST=($($AWS iam list-role-policies --role-name ${IAM_ROLE} |jq '.PolicyNames[]' | tr -d '"'))
-    for POLICY_NAME in "${POLICY_NAME_LIST[@]}"
-    do
+    for POLICY_NAME in "${POLICY_NAME_LIST[@]}"; do
         echo "Deleting Role Policy ${POLICY_NAME}"
         $AWS iam delete-role-policy --role-name ${IAM_ROLE} --policy-name ${POLICY_NAME}
     done
@@ -146,14 +161,14 @@ do
     $AWS ec2 delete-nat-gateway --nat-gateway-id "${NAT_GATEWAY}"
 done
 
-EIP_ASSOCIATION_LIST=($($AWS ec2 describe-addresses | jq '.Addresses[] | select((.Tags[] | select(.Key | contains("aws:cloudformation:stack-name"))) == 0) | .AssociationId' | tr -d '"'))
+EIP_ASSOCIATION_LIST=($($AWS ec2 describe-addresses | jq '.Addresses[] | .AssociationId | select( . != null )' | tr -d '"'))
 for ASSOCIATION in "${EIP_ASSOCIATION_LIST[@]}"
 do
     echo "Disassociate association ${ASSOCIATION}"
     $AWS ec2 disassociate-address --association-id "${ASSOCIATION}"
 done
 
-EIP_ALLOCATION_LIST=($($AWS ec2 describe-addresses | jq '.Addresses[] | select((.Tags[] | select(.Key | contains("aws:cloudformation:stack-name"))) == 0) | .AssociationId' | tr -d '"'))
+EIP_ALLOCATION_LIST=($($AWS ec2 describe-addresses | jq '.Addresses[] | .AllocationId | select( . != null )' | tr -d '"'))
 for ALLOCATION in "${EIP_ALLOCATION_LIST[@]}"
 do
     echo "Releasing allocation ${ALLOCATION}"
@@ -191,7 +206,7 @@ do
     $AWS ec2 delete-vpc --vpc-id "${VPC}"
 done
 
-SERVICE_DISCOVERY_LIST=$($(AWS servicediscovery list-services | jq '.Services[] | .Id' | tr -d '"'))
+SERVICE_DISCOVERY_LIST=($($AWS servicediscovery list-services | jq '.Services[] | .Id' | tr -d '"'))
 for SERVICE_DISCOVERY in "${SERVICE_DISCOVERY_LIST[@]}"
 do
     echo "Deleting service discovery service ${SERVICE_DISCOVERY}"
